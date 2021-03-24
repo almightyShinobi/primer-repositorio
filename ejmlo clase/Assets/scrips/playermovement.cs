28f85f0189bd07a6b4876a0a22124f4834ce126a
almightyShinobi <molinaitalo4@gmail.com>
2021-03-24 19:55:02 -0300

    update ejemplo
    agrege solidificacion de obstaculos  camara y salto

--- ejmlo clase/Assets/scrips/playermovement.cs
+++ ejmlo clase/Assets/scrips/playermovement.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class playermovement : MonoBehaviour
 {
 
-    public float Speed=1.0f;
+    public float Speed = 1.0f;
+    public float RotationSpeed = 1.0f;
+    public float JumpForce = 1.0f;
+
+    private Rigidbody Physics;
+
     
     
     // Start is called before the first frame update
     void Start()
     {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
+        Physics = GetComponent<Rigidbody>();
+
+
+      
         
     }
 
@@ -21,6 +34,19 @@ public class playermovement : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(horizontal,0.0f,vertical)*Time.deltaTime*Speed);
+
+        float rotationY = Input.GetAxis("Mouse X");
+
+        transform.Rotate(new Vector3(0, rotationY*Time.deltaTime*RotationSpeed , 0));
+
+        //salto
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Physics.AddForce(new Vector3(0, JumpForce,0), ForceMode.Impulse);
+            
+        }
+
+        
 
 
     }
